31b52443406c9dd659bf727612f94b9f96c1f80a
unknown <08250277.sce@rub.edi.bt>
2026-05-18 17:56:24 +0600

    G4.py

--- G4.py
+++ G4.py
@@ -4,3 +4,25 @@ count = 0
 # Variables for first and last student
 first_student = ""
 last_student = ""
+
+# Read file line by line
+for line in file:
+
+    # Check if student ID ends with 8 or 9
+    if line[-2] == "8" or line[-2] == "9":
+
+        # Display student record
+        print(line)
+
+        # Store record in output file
+        output.write(line)
+
+        # Count total students
+        count = count + 1
+
+        # Store first student
+        if count == 1:
+            first_student = line
+
+        # Store last student
+        last_student = line
